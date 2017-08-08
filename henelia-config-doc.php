3e5e56c9d2d052734b7b499e1f46656bee9a15d2
Guillard Jean-Baptiste <jb.guillard@henelia.fr>
2017-08-08 12:10:30 +0200

    Revert "Revert "Mise à jour de la page documentation. Ajout des liens vers Github et bureau des réclamations.""
    This reverts commit b2c35cba6e6c85914a8a5c22f017371919931207.

--- henelia-config-doc.php
+++ henelia-config-doc.php
@@ -13,5 +13,17 @@
 	<div class="wrap">
 		<h1>Henelia Documentation</h1>
 		<h2>Retrouver ici toutes la documentation du theme et des plugins Henelia</h2>
+		<p>La documentation est en évolution constante, pensez à regarder souvent.</p>
+		<h3>Bureau des réclamations</h3>
+		<p>Vous avez des demandes d'évolutions, vous découvrer un bugs, remonté tous ca au <a href="https://docs.google.com/spreadsheets/d/1hLmFOMP4f2ZO93TpYF72_gR9h3CzkxMtCWUSPJF3DdQ/edit?usp=sharing" target="_blank" class="button button-primary">bureau des réclamations</a>.</p>
+
+		<h2>Github</h2>
+		<p>Pour installer les plugins, thèmes, ... [He]nelia, voir les documentations, les commit et peut etre contribuer aux évo c'est ici :</p>
+		
+		<p>
+		<a href="https://github.com/henelia" target="_blank" class="button button-primary">Github</a>&nbsp;&nbsp;&nbsp;
+		<a href="https://github.com/henelia/heneliaWP" target="_blank" class="button button-secondary">Henelia WP theme</a>&nbsp;&nbsp;&nbsp;
+		<a href="https://github.com/henelia/henelia-config" target="_blank" class="button button-secondary">Henelia config - Plugins</a>
+		</p>
 	</div>
 <?php } ?>
